6d38a03f530ae56178577c614fb7d0b53c7ac7f9
aejester <aftereffectsjester@gmail.com>
2021-12-15 11:48:29 -0500

    Add ServerProperties.java to be worked on at a later date

---
+++ src/main/java/com/trentberrien/civmaker/ServerProperties.java
@@ -0,0 +1,13 @@
+package com.trentberrien.civmaker;
+
+public class ServerProperties {
+
+    //TODO: SERVER PROPERTIES
+    //List as variables here
+    //Dont include them in the constructor
+
+    public ServerProperties() {
+        
+    }
+
+}
